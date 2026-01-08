bf4a8e60507693ede50905c2fbb340e05d99b841
Bharath Kumar <bharath.kumar@bidryde.in>
2026-01-08 17:52:47 +0530

    ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,20 +1,26 @@
 import UIKit
 import Flutter
+import Firebase // Add this
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
-
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-
-    // DO NOT call FirebaseApp.configure() here
-
-
+    
+    // Register for remote notifications
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
-  
+  // Add this method to pass the APNs token to Firebase
+  override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+    Messaging.messaging().apnsToken = deviceToken
+    super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
+  }
 }
